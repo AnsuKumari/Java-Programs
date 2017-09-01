c658e65f62e8d4ef16985dff48c58ddd6d7e4e76
Ansu Kumari <ansu.kumari@outlook.com>
2017-09-01 12:49:29 +0530

    Update Pass.java

--- src/Pass.java
+++ src/Pass.java
@@ -1,1 +1,31 @@
+class Data{
+	int a;
 
+	Data(int x){
+		a = x;
+	}
+	
+	int Ref(Data D){
+		D.a = 50;
+		return D.a;
+	}
+
+	int Val(int a){
+		a = 50;
+		return a;
+	}
+	
+}
+
+class Pass{
+
+	public static void main(String args[]){
+		int a = 2;
+		Data d = new Data(2);
+
+		System.out.println("Passed by value");
+		System.out.println("Function output:- " +  d.Val(a) + " a:- " + a + "\n");
+		System.out.println("Passed by reference:- ");
+		System.out.println("Function output:- " + d.Ref(d) + " d.a:- " + d.a);
+	}
+}
